7fbe76f18405057fbc4873936611bd368215f562
Rayan Malik <rayan.malik@duke.edu>
2022-06-13 18:38:39 -0400

    Completed test cases

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profileImage from "../assets/RayanMalik.JPG";
   
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <p>Design your About me page </p> 
+<div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={profileImage}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Rayan Malik</div>
+            <div className="brief_description">
+              I am an undegraduate student at Duke University double majoring in Computer Science and Economics. 
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
